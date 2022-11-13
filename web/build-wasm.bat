1a824f700ce6d9cc36cd6739614952cf8579ceee
Daniel Stotts <dstodev@gmail.com>
2022-11-13 00:53:53 -0800

    cleanup

--- web/build-wasm.bat
+++ web/build-wasm.bat
@@ -1,19 +1,18 @@
 @echo off
 
 set "script_dir=%~dp0"
-set "source_dir=%script_dir%.."
-
 set "build_dir=%1"
+
 if "%build_dir%" == "" set "build_dir=%script_dir%"
 
 set "target=wasm32-unknown-unknown"
 
 rustup target add wasm32-unknown-unknown
 
-:: https://stackoverflow.com/questions/62585840/web-assembly-and-rust-cargo-build-vs-wasm-pack
-
-::cargo build --release --manifest-path %source_dir%\Cargo.toml --target=%target%
-::copy %source_dir%\target\%target%\release\wasm_rs.wasm %build_dir%\rs.wasm
+:: pushd/popd and %cd% for better handling of relative paths
+pushd %build_dir%
+set "build_dir=%cd%"
+popd
 
 :: requires wasm-pack: https://github.com/rustwasm/wasm-pack
-wasm-pack --quiet build --target web --out-dir %build_dir%pkg
+wasm-pack --quiet build --target web --out-dir "%build_dir%\pkg"
